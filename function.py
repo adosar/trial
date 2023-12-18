43142dcebab72d8106738e7a4c6b7a37b2e5f0a2
Antonios Sarikas <antonios.sarikas@gmail.com>
2023-12-18 21:51:31 +0200

    Change f(x) to multiply by 6

--- function.py
+++ function.py
@@ -1,10 +1,10 @@
 def f(x):I am attaching the CoRE database (the .cif files).
     r"""
-    Return the multiple of input by 2.
+    Return the multiple of input by 6.
     Customize the function.
     """
     print('On version 2.0.0')
-    return 2*x
+    return 6*x
 
 
 __version__ = "2.0.0"
