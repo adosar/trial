e8b28df828db8262839f4ba0f25107da880c53c9
Antonios Sarikas <antonios.sarikas@gmail.com>
2024-07-26 12:49:13 +0300

    Renaming

---
+++ fu.py
@@ -0,0 +1,27 @@
+def f(x):
+    """
+    Return the multiple of i\nput by 6.
+
+    Extented descriptions of the function.
+
+    Parameters
+    ----------
+    x : float
+        Input value
+
+    Returns
+    -------
+    float
+        The output value
+    """
+    print('On version 2.0.0')
+    return 6*x
+
+
+__version__ = "2.0.0"
+__author__ = "Foo Bar"
+__tag__ = 'THIS IS THE TAG'
+
+
+def g(x):
+    return 2
